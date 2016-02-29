fbf2040a23139647ad482e6e0ec2aa2df8d3cafb
sreveloc <sreveloc@gmail.com>
2016-03-01 00:41:07 +0100

    Añado el webclient (Frontend)

---
+++ brofistfrfight-webclient/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name brofistfrfightApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the brofistfrfightApp
+ */
+angular.module('brofistfrfightApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
